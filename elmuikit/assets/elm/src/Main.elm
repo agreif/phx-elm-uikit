a8bab7f29accd9f7fed4c8f0250217cb88f2b1e0
Alex Greif <alex.greif@gmail.com>
2022-12-18 13:40:31 +0100

    use elm Browser.application

--- elmuikit/assets/elm/src/Main.elm
+++ elmuikit/assets/elm/src/Main.elm
@@ -1,7 +1,101 @@
 module Main exposing (..)
-import Html exposing (text)
 
-name = "John"
+import Browser
+import Browser.Navigation as Nav
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Url
 
+
+
+-- MAIN
+
+
+main : Program (Maybe String) Model Msg
 main =
-  text ("Hello " ++ name ++ "!")
+  Browser.application
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    , onUrlChange = UrlChanged
+    , onUrlRequest = LinkClicked
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { key : Nav.Key
+  , url : Url.Url
+  }
+
+
+init : Maybe String -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init maybeUrlStr url key =
+  case maybeUrlStr of
+    Just urlStr ->
+      case Url.fromString urlStr of
+        Just url2 -> ( Model key url2, Nav.pushUrl key (Url.toString url2) )
+        _ -> ( Model key url, Cmd.none )
+    _ -> ( Model key url, Cmd.none )
+
+
+-- UPDATE
+
+
+type Msg
+  = LinkClicked Browser.UrlRequest
+  | UrlChanged Url.Url
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    LinkClicked urlRequest ->
+      case urlRequest of
+        Browser.Internal url ->
+          ( model, Nav.pushUrl model.key (Url.toString url) )
+
+        Browser.External href ->
+          ( model, Nav.load href )
+
+    UrlChanged url ->
+      ( { model | url = url }
+      , Cmd.none
+      )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document Msg
+view model =
+  { title = "URL Interceptor"
+  , body =
+      [ text "The current URL is: "
+      , b [] [ text (Url.toString model.url) ]
+      , ul []
+          [ viewLink "/home"
+          , viewLink "/profile"
+          ]
+      ]
+  }
+
+
+viewLink : String -> Html msg
+viewLink path =
+  li [] [ a [ href path ] [ text path ] ]
+
--- elmuikit/assets/elm/src/index.js
+++ elmuikit/assets/elm/src/index.js
@@ -4,5 +4,7 @@ const $root = document.createElement('div');
 document.body.appendChild($root);
 
 Elm.Main.init({
-  node: $root
+    node: $root,
+    //flags: null,
+    flags: "http://localhost:4000/profile"
 });
